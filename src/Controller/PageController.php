c8d596f36f89fffc038d0eb04bc725374b139474
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-12-06 16:01:36 +0100

    Tableau article

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,6 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    private $article = [
+        1 => [
+            "titre" => "Titre 1",
+            "contenu" => "Lorem ipsum dolor sit,
+             amet consectetur adipisicing elit. 
+             Voluptates, officiis praesentium amet vitae perspiciatis id, 
+             ducimus natus optio in molestiae, 
+             placeat harum veniam eaque obcaecati tempore adipisci ratione quos ut!"
+        ],
+        2 => [
+            "titre" => "Titre 2",
+            "contenu" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error quisquam, doloribus tenetur minima recusandae amet obcaecati nisi omnis ullam accusantium quibusdam commodi iste sapiente incidunt unde dolore, sunt ducimus doloremque."
+        ],
+        3 => [
+            "titre" => "Titre 2",
+            "contenu" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit magni quisquam accusamus! Dolore rerum fugit praesentium iste fugiat voluptatem ducimus sunt sit! Doloremque nisi ratione ex quod natus impedit id."
+        ]
+    ];
     /**
      * @Route("/page", name="page")
      */
@@ -23,8 +41,7 @@ class PageController extends AbstractController
      */
     public function home()
     {
-        var_dump("home");
-        die;
+        return new Response("home");
     }
 
     // créer un deuxième route , legal, qui va afficher le texte : mentions légales du sites
@@ -34,7 +51,23 @@ class PageController extends AbstractController
      */
     public function legal()
     {
-        var_dump("mentions légales du site");
-        die;
+        return new Response('Mentions légales du site');
+    }
+
+    /**
+     * @Route("/toto/", name="toto")
+     */
+    public function toto()
+    {
+        return new Response('Toto est très bête.');
+    }
+
+    /**
+     * @Route("/number/{id}", name="number")
+     */
+    public function number($id)
+    {
+        $this->article;
+        return new Response($id);
     }
 }
